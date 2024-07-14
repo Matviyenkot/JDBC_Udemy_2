24f0f622f364d0e1c96583eeefd8fbc2c133d41e
tarasmatvienko <matviyenkot@gmail.com>
2024-07-14 21:32:15 +0300

    blob&clob

---
+++ src/main/java/org/example/BlobRunner.java
@@ -0,0 +1,66 @@
+package org.example;
+
+import org.example.util.ConnectionManager;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.sql.*;
+
+public class BlobRunner {
+    public static void main(String[] args) throws SQLException, IOException {
+        //blob - bytea
+        //clob - TEXT
+        getImage();
+    }
+
+    private static void getImage() throws SQLException, IOException {
+        var sql = """
+                select image
+                from practice.aircraft
+                where id = ?
+                """;
+
+        try(Connection connection = ConnectionManager.open();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+            preparedStatement.setInt(1, 1);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                byte[] images = resultSet.getBytes("image");
+                Files.write(Path.of("resources", "boeing777_new.jpg"), images, StandardOpenOption.CREATE);
+            }
+        }
+    }
+
+    private static void saveImage() throws SQLException, IOException {
+        var sql = """
+                update practice.aircraft 
+                set image = ?
+                where id = 1
+                """;
+        try (Connection connection = ConnectionManager.open();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setBytes(1, Files.readAllBytes(Path.of("resources", "boeing777.jpg")));
+            preparedStatement.executeUpdate();
+        }
+    }
+
+//    private static void saveImage() throws SQLException, IOException {
+//        var sql = """
+//                update practice.aircraft
+//                set image = ?
+//                where id = 1
+//                """;
+//        try (Connection connection = ConnectionManager.open();
+//             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+//            connection.setAutoCommit(false);
+//            Blob blob = connection.createBlob();
+//            blob.setBytes(1, Files.readAllBytes(Path.of("resources", "boeing777.jpg")));
+//
+//            preparedStatement.setBlob(1, blob);
+//            preparedStatement.executeUpdate();
+//            connection.commit();
+//        }
+//    }
+}
